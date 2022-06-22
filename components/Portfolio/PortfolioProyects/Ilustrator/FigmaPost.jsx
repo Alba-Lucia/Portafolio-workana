9b93eece96bcf28971e19506ce6d5cc3ffdc4926
Alba Lucia Gonzalez <alba.gonzalezibarguen@gmail.com>
2022-06-21 19:22:35 -0500

    change

--- components/Portfolio/PortfolioProyects/Ilustrator/FigmaPost.jsx
+++ components/Portfolio/PortfolioProyects/Ilustrator/FigmaPost.jsx
@@ -8,25 +8,31 @@ const Ilustrator = () => {
          <h2 className={styles.entrada__title}>Photoshop / Ilustrator</h2>
             <div className={styles.entrada__container}>
                
-               <div className={styles.entrada__content}>
-                  <img
-                     className={styles.entrada__image}
-                     src='/Gift/Gif-1.gif' 
-                     alt='image Figma web Waseskun'
-                  />
+            <div className={styles.entrada__content}>
+                  <div className={styles.entrada__image}>
+                     <Image
+                        height='400px'
+                        width='400px'
+                        src='/Gift/Gif-1.gif' 
+                        alt='image Figma web Waseskun'
+                     />
+                  </div>
                </div>
                <div className={styles.entrada__content}>
-                  <img
-                     className={styles.entrada__image}
-                     src='/Gift/Gif-2.gif' 
-                     alt='image Figma web Waseskun'
-                  />
+                  <div className={styles.entrada__image}>
+                     <Image
+                        height='400px'
+                        width='400px'
+                        src='/Gift/Gif-2.gif' 
+                        alt='image Figma web Waseskun'
+                     />
+                  </div>
                </div>
                <div className={styles.entrada__content}>
                  <div className={styles.entrada__image}>
                  <Image
                      height='400px'
-                     width='300px'
+                     width='400px'
                      src='/ilustrations/ilustracionPortfolio6.svg' 
                      alt='image Figma web Waseskun'
                   />
